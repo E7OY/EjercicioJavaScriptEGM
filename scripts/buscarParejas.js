8d88f447a6f60eae12a252902d92508332660780
soyel <soyeloygarciamartinez@gmail.com>
2024-11-18 10:46:38 +0100

    crear funcion para generar tablero

--- scripts/buscarParejas.js
+++ scripts/buscarParejas.js
@@ -3,4 +3,28 @@ let mostrarAciertos = document.getElementById('aciertos');
 let tablero = document.getElementById('tablero');
 let mensajeResultado = document.getElementById('mensaje_resultado');
 let formDificultad = document.getElementById('form_dificultad');
+let personalizadoBoton = document.getElementById('personalizado');
+let seleccionarDificultad = document.getElementById('seleccionar_dificultad');
 let mensajeError = document.getElementById('mensaje_error');
+
+
+let filas = 3;
+let columnas = 4;
+
+
+function generarTablero() {
+    tablero.innerHTML = '';
+    for(let i = 0; i<filas; i++){
+        let fila = document.createElement('tr');
+        for(let j = 0; j<columnas; j++) {
+            let tarjeta = document.createElement('td');
+            let boton = document.createElement('button');
+            boton.className = 'tarjeta casilla';
+            boton.id = i * columnas + j;
+            boton.setAttribute('onclick', `destapar(${boton.id})`);
+            fila.appendChild(tarjeta); 
+            tarjeta.appendChild(boton);
+        } 
+        tablero.appendChild(fila);
+    }
+}
