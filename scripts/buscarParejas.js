0a15bc124c27e05db4c8d7f5ac6ff33c6211b510
soyel <soyeloygarciamartinez@gmail.com>
2024-11-18 10:30:27 +0100

    empezar referenciando elementos html en el js

---
+++ scripts/buscarParejas.js
@@ -0,0 +1,6 @@
+let mostrarIntentos = document.getElementById('intentos');
+let mostrarAciertos = document.getElementById('aciertos');
+let tablero = document.getElementById('tablero');
+let mensajeResultado = document.getElementById('mensaje_resultado');
+let formDificultad = document.getElementById('form_dificultad');
+let mensajeError = document.getElementById('mensaje_error');
