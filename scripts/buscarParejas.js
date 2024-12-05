9e0e8a37fb4d75e81597f3bf45b02019064ce501
soyel <soyeloygarciamartinez@gmail.com>
2024-12-05 12:33:59 +0100

    git breakout para quitar funcionalidad de cambiar imagenes por conflictos con otras funcionalidades

--- scripts/buscarParejas.js
+++ scripts/buscarParejas.js
@@ -15,52 +15,23 @@ let tarjeta2 = null;
 let segundoNumeroDestapado = null;
 let aciertos = 0;
 let numeros = [];
-
-
-let imagenOriginal = 'img/favicon.png';
-let imagenAlternativa = 'img/favicon2.png';
-let imagenAlternativaActiva = false;
-
-document.querySelectorAll('input[name="selector-imagenes"]').forEach((radio) => {
-    radio.addEventListener('change', (event) => {
-        imagenAlternativaActiva = event.target.value === 'alternativa';
-        resetearTarjetas();
-    });
-});
-
-
 let partidasGanadas = document.getElementById('partidas_ganadas');
 let ganadas = localStorage.getItem('partidasGanadas');
 partidasGanadas.innerHTML = `Partidas ganadas: ${ganadas}`;
 
-
-let tarjetasAcertadas = [];
-function resetearTarjetas() {
-    let tarjetas = document.querySelectorAll('.tarjeta');
-    let imagen = imagenAlternativaActiva ? 'img/favicon2.png' : 'img/favicon.png';
-    tarjetas.forEach((tarjeta, index) => {
-        if (!tarjetasAcertadas.includes(index)) {
-            tarjeta.innerHTML = `<img src="${imagen}" alt="imagen de tarjeta" width="100%">`;
-            tarjeta.disabled = false;
-        }
-    });
-}
-
-
-
 function generarTablero() {
     tablero.innerHTML = '';
-    for (let i = 0; i < filas; i++) {
+    for(let i = 0; i<filas; i++){
         let fila = document.createElement('tr');
-        for (let j = 0; j < columnas; j++) {
+        for(let j = 0; j<columnas; j++) {
             let tarjeta = document.createElement('td');
             let boton = document.createElement('button');
             boton.className = 'tarjeta casilla';
             boton.id = i * columnas + j;
             boton.setAttribute('onclick', `destapar(${boton.id})`);
-            fila.appendChild(tarjeta);
+            fila.appendChild(tarjeta); 
             tarjeta.appendChild(boton);
-        }
+        } 
         tablero.appendChild(fila);
     }
 }
@@ -74,81 +45,81 @@ function mezclarNumeros(numeros) {
 }
 
 
-
 function iniciarJuego() {
     numeros = [];
-    for (let i = 1; i <= (filas * columnas) / 2; i++) {
+    for(let i = 1; i<=(filas * columnas) / 2; i++) {
         numeros.push(i);
         numeros.push(i);
     }
     numeros = mezclarNumeros(numeros);
 
-    const parejasRestantes = numeros.length / 2;
+    const parejasRestantes = numeros.length/2;
 
     mostrarIntentos.innerHTML = `Numero de intentos: ${intentos}`;
     mostrarAciertos.innerHTML = `Parejas restantes: ${parejasRestantes}`;
 
-    tarjetasAcertadas = [];
     resetearTarjetas();
+}
+
+function resetearTarjetas(){
+    let tarjetas = document.querySelectorAll('.tarjeta');
+    tarjetas.forEach(tarjeta => {
+        tarjeta.innerHTML = '<img src="img/favicon.png" alt="imagen de tarjeta" width="100%">';
+        tarjeta.disabled = false;
+    });
 }
 
 function destapar(id) {
     tarjetasDestapadas++;
 
-    if (tarjetasDestapadas == 1) {
+    if(tarjetasDestapadas == 1){
         tarjeta1 = document.getElementById(id);
         primerNumeroDestapado = numeros[id];
-        let imagen = imagenAlternativaActiva ? 'img/favicon2.png' : 'img/favicon.png';
         tarjeta1.innerHTML = primerNumeroDestapado;
         tarjeta1.disabled = true;
-    } else if (tarjetasDestapadas == 2) {
+    } else if(tarjetasDestapadas == 2) {
         tarjeta2 = document.getElementById(id);
         segundoNumeroDestapado = numeros[id];
-        let imagen = imagenAlternativaActiva ? 'img/favicon2.png' : 'img/favicon.png';
         tarjeta2.innerHTML = segundoNumeroDestapado;
         tarjeta2.disabled = true;
         intentos++;
         mostrarIntentos.innerHTML = `Numero de intentos: ${intentos}`;
 
-        if (primerNumeroDestapado == segundoNumeroDestapado) {
+        if(primerNumeroDestapado == segundoNumeroDestapado) {
             tarjetasDestapadas = 0;
             aciertos++;
-            tarjetasAcertadas.push(parseInt(tarjeta1.id));
-            tarjetasAcertadas.push(parseInt(tarjeta2.id));
             const parejasRestantes = (numeros.length / 2) - aciertos;
             mostrarAciertos.innerHTML = `Parejas restantes: ${parejasRestantes}`;
-            mensajeResultado.innerHTML = `Los números destapados son iguales.`;
+            mensajeResultado.innerHTML = `Los numeros destapados son iguales.`;
             mensajeResultado.style.display = 'block';
             mensajeResultado.style.color = '#7ED4AD';
             tarjeta1.style.backgroundColor = '#7ED4AD';
             tarjeta2.style.backgroundColor = '#7ED4AD';
-
-            if (aciertos == (numeros.length / 2)) {
-                mostrarAciertos.innerHTML = `Parejas restantes: 0 ¡Felicidades!`;
+            if(aciertos == (numeros.length/2)) {
+                mostrarAciertos.innerHTML = `Parejas restantes: 0.`;
                 mostrarIntentos.innerHTML = `Juego completado en ${intentos} intentos.`;
-
                 ganadas++;
                 localStorage.setItem('partidasGanadas', ganadas);
                 partidasGanadas.innerHTML = `Partidas ganadas: ${ganadas}`;
-
                 confetti({
                     particleCount: 300,
                     spread: 200,
                     origin: { y: 0.8 }
                 });
             }
+            
         } else {
             mensajeResultado.innerHTML = `Los números destapados son distintos.`;
             mensajeResultado.style.display = 'block';
             mensajeResultado.style.color = '#F95454';
             tarjeta2.style.backgroundColor = '#F95454';
             tarjeta1.style.backgroundColor = '#F95454';
-            setTimeout(() => {
+            setTimeout(() =>{
                 tarjeta2.style.backgroundColor = '#00000057';
                 tarjeta1.style.backgroundColor = '#00000057';
                 tarjetasDestapadas = 0;
-                tarjeta1.innerHTML = `<img src="${imagen}" alt="imagen de tarjeta" width="100%">`;
-                tarjeta2.innerHTML = `<img src="${imagen}" alt="imagen de tarjeta" width="100%">`;
+                tarjeta1.innerHTML = '<img src="img/favicon.png" alt="imagen de tarjeta" width="100%">';
+                tarjeta2.innerHTML = '<img src="img/favicon.png" alt="imagen de tarjeta" width="100%">';
                 tarjeta1.disabled = false;
                 tarjeta2.disabled = false;
             }, 1000);
@@ -158,6 +129,7 @@ function destapar(id) {
         }, 1000);
     }
 }
+
 
 seleccionarDificultad.addEventListener('click', () => {
     const dificultad = formDificultad.dificultad.value;
@@ -189,8 +161,8 @@ seleccionarDificultad.addEventListener('click', () => {
                 mensajeError.innerHTML = 'El número de casillas debe ser par.';
                 setTimeout(() => {
                     mensajeError.style.display = 'none';
-                 }, 5000);
-             return;
+                }, 5000);
+            return;
         }
     }
 
@@ -198,6 +170,15 @@ seleccionarDificultad.addEventListener('click', () => {
     iniciarJuego();
 });
 
- 
+formDificultad.dificultad.forEach(radio => {
+    radio.addEventListener('change', () => {
+        if(radio.value === 'personalizado') {
+            document.getElementById('personalizado').style.display = 'block';
+        } else {
+            document.getElementById('personalizado').style.display = 'none';
+        }
+    });
+});
+
 generarTablero();
 iniciarJuego();
